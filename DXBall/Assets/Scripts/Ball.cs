3f80678f7326f473f1d810625189cc29cc4a3668
Kaley Carrubba <kcarrubba@rollins.edu>
2025-09-10 11:50:58 -0400

    add side walls/more bricks

--- DXBall/Assets/Scripts/Ball.cs
+++ DXBall/Assets/Scripts/Ball.cs
@@ -15,11 +15,27 @@ public class ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity= direction*speed;
+        rb.linearVelocity= direction * speed;
     }
 
-    void OnTriggerEnter2D(Collider2D collison){
+    void OnTriggerEnter2D(Collider2D collison)
+    {
         if (collison.gameObject.CompareTag("Paddle"))
             direction.y = -direction.y;
+
+        else if (collison.gameObject.CompareTag("Brick"))
+        {
+            direction.y = -direction.y;
+            Destroy(collison.gameObject);
+        }
+
+        else if (collison.gameObject.CompareTag("Side Wall"))
+            direction.x = -direction.x;
+
+        else if (collison.gameObject.CompareTag("Top Wall"))
+            direction.y = -direction.y;
+
+        else if (collison.gameObject.CompareTag("Bottom Wall"))
+            Debug.Log("Game over");
     }
 }
