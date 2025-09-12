eb70d693a39c17f98d5c188c6440e8e4a454d613
Kaley Carrubba <kcarrubba@rollins.edu>
2025-09-12 15:50:45 -0400

    finish creating scoreboard and win/lose text

--- DXBall/Assets/Scripts/ScoreManager.cs
+++ DXBall/Assets/Scripts/ScoreManager.cs
@@ -5,6 +5,7 @@ public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI winText;
+    public GameObject ball;
     int score = 0;
 
     public void addScore(int input)
@@ -12,9 +13,14 @@ public class ScoreManager : MonoBehaviour
         if (input == 1) {
             score = score + input;
             scoreText.text = "Score: " + score.ToString();
+            if (score == 60) {
+            winText.text = "You Win!";
+            ball.SetActive(false);
+            }
         }
         else if (input == 0) {
             winText.text = "Game Over!";
+
         }
     }
 }
