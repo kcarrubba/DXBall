7ddca8c11ee66d1c6517f15252b2f36f169b293b
Kaley Carrubba <kcarrubba@rollins.edu>
2025-09-15 11:41:31 -0400

    adjusted text

--- DXBall/Assets/Scripts/ScoreManager.cs
+++ DXBall/Assets/Scripts/ScoreManager.cs
@@ -14,7 +14,7 @@ public class ScoreManager : MonoBehaviour
         if (input == 1) {
             score = score + input;
             scoreText.text = "Score: " + score.ToString();
-            if (score == 1) {
+            if (score == 60) {
                 winText.text = "You Win!";
                 ball.SetActive(false);
                 gameOver = true;
